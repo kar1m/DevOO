be8413dffe508625ecf768165f3e0536e64d64e9
Karim <karim2904@gmail.com>
2014-11-26 11:16:47 +0100

    Choco init

--- Implementation/Optimod_Lyon/src/Modele/Chemin.java
+++ Implementation/Optimod_Lyon/src/Modele/Chemin.java
@@ -2,6 +2,14 @@ package Modele;
 
 import java.util.Vector;
 
+import solver.ResolutionPolicy;
+import solver.Solver;
+import solver.constraints.IntConstraintFactory;
+import solver.search.loop.monitors.SearchMonitorFactory;
+import solver.search.strategy.IntStrategyFactory;
+import solver.variables.IntVar;
+import solver.variables.VariableFactory;
+
 /**
  * 
  */
@@ -10,7 +18,7 @@ public class Chemin {
 	private Livraison origine;
     private Livraison destination;
     private Vector<Troncon> listeTroncons;
-	
+    private Graph graph;
     /**
      * 
      */
